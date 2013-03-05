563006d119288d86f0dddd58ec49cc34989f61f9
Philip Lewis <pcl@pclewis.com>
2013-03-04 21:08:27 -0500

    findFirstHeader -> findNextHeader

--- dc.c
+++ dc.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <stdarg.h>
 #include <stdint.h>
+#include <stdbool.h>
 
 #define BLOCK_SIZE  0x1E00
 #define BITRATE     128000
@@ -18,16 +19,24 @@ void die(char *fmt, ...) {
 	exit(EXIT_FAILURE);
 }
 
-size_t findFirstFrameHeader(uint8_t *data) {
-	for(size_t i = 0; i < 32; ++i) {
-		if( data[i] == data[LOOP_OFFSET+i] &&
-			data[i+1] == data[LOOP_OFFSET+i+1]) {
-			return i;
+size_t findNextFrameHeader(uint8_t *data, size_t size, size_t start) {
+	size_t i = 0;
+
+	for(i = start; i < size; ++i) {
+		if( data[i] == data[LOOP_OFFSET+i] && data[i+1] == data[LOOP_OFFSET+i+1]) {
+			uint8_t b1 = data[i], b2 = data[i+1];
+			bool found = true;
+			for(size_t j = i + LOOP_OFFSET; j < size; j += LOOP_OFFSET) {
+				if(data[j] != b1 || data[j+1] != b2) {
+					found = false;
+					break;
+				}
+			}
+			if(found) break;
 		}
 	}
 
-	die("Could not find first frame header");
-	return (size_t)-1;
+	return i;
 }
 
 int main(int argc, char *argv[]) {
@@ -42,14 +51,17 @@ int main(int argc, char *argv[]) {
 		return EXIT_FAILURE;
 	}
 
-	void *data = malloc( ftell(fp) );
-	fread(data, ftell(fp), 1, fp);
+	size_t size = ftell(fp);
+	void *data = malloc( size );
+	fread(data, size, 1, fp);
 	fclose(fp);
 
 
-	size_t firstFrameHeader = findFirstFrameHeader(data);
+	size_t firstFrameHeader = findNextFrameHeader(data, size, 0);
+	size_t secondFrameHeader = findNextFrameHeader(data, size, firstFrameHeader+FRAME_SIZE);
 
 	printf("Found first frame at %zu\n", firstFrameHeader);
+	printf("Found second frame at %zu\n", secondFrameHeader);
 
 	return EXIT_SUCCESS;
 }
